79a4d3ed8482e54be1444ab0be0f0a06b689e276
mrsam468 <ibenjaminsamuel47@gmail.com>
2026-06-03 10:11:28 +0100

    restructuring hospital management

---
+++ src/test/java/hospitalrecords/PatientTest.java
@@ -0,0 +1,42 @@
+package hospitalrecords;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientTest {
+    Patient patient;
+
+    @BeforeEach
+    void init(){
+        patient = new Patient("Daniel","Abel","kate", 2, 24, "malaria", 5000);
+    }
+
+    @Test
+    void getFullName() {
+        assertEquals("Daniel Abel kate",patient.getFullName());
+    }
+
+
+    @Test
+    void getPatientId() {
+        assertEquals(2,patient.getPatientId());
+    }
+
+    @Test
+    void getAge() {
+        assertEquals(24,patient.getAge());
+    }
+
+    @Test
+    void getIllnessName() {
+        assertEquals("malaria",patient.getIllnessName());
+    }
+
+    @Test
+    void getWalletBalance() {
+        assertEquals(5000,patient.getWalletBalance());
+    }
+
+}
